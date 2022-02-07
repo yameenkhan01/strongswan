cf10e2e64381c7729a36f3dd197cfd51c79c419f
Tobias Brunner <tobias@strongswan.org>
2022-02-07 14:21:43 +0100

    child-cfg: Add flag to enable ICMP forwarding

--- src/libcharon/config/child_cfg.h
+++ src/libcharon/config/child_cfg.h
@@ -345,6 +345,9 @@ enum child_cfg_option_t {
 
 	/** Disable copying the ECN header field in tunnel mode */
 	OPT_NO_COPY_ECN = (1<<8),
+
+	/** Enable automatic forwarding of certain ICMP errors */
+	OPT_FORWARD_ICMP = (1<<9),
 };
 
 /**
